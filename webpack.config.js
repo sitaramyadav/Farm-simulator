7f04ce29394fa2ffb6d9d7ea77cb10908c5e41ea
in-rahul.patil <rahulpatil-99>
2019-04-20 22:35:36 +0530

    Rahul - initial setup

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/js/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports =  {
+    entry:  __dirname + '/js/index.js',
+    output: {
+        path: __dirname + '/out',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".css"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader',
+                })
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    devServer: {
+        port: 3000,
+        host: "0.0.0.0",
+        hot: true,
+        inline: true,
+        disableHostCheck: true,
+        historyApiFallback: true,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+        }
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css'),
+        HTMLWebpackPluginConfig,
+    ]
+};
