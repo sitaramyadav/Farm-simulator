10fe438084f00055104267d8acc2aae55181a82c
in-rahul.patil <rahulpatil-99>
2019-04-21 23:10:58 +0530

    Rahul - Add static farm and change css properties

--- js/Components/MainWindow.js
+++ js/Components/MainWindow.js
@@ -2,12 +2,14 @@ import React, {Component} from "react";
 import '../CSS/mainWindow.css'
 import {Sun} from "./Sun";
 import {Sky} from "./Sky";
+import {Farm} from "./Farm";
 
 export class MainWindow extends Component {
     render() {
         return <div className="Main-window ">
             <Sky/>
             <Sun/>
+            <Farm/>
         </div>;
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {index} from  "./Components/App";
-import {MainWindow} from "./Components/MainWindow";
-import {ButtonPane} from "./Components/ButtonPane";
+import App from  "./Components/App";
 
-ReactDOM.render(<div className='App'>
-    <MainWindow/>
-    <ButtonPane/>
-    </div>
+ReactDOM.render(<App />
 , document.getElementById("content"));
