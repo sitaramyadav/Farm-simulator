4c94b1e3e4d9016c655372b128001456ef2d5e07
in-rahul.patil <rahulpatil-99>
2019-04-23 15:22:57 +0530

    Rahul - Add Clouds component

---
+++ js/Components/Clouds.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import "../CSS/Clouds.css";
+
+export class Clouds extends Component {
+    render() {
+        return <div className="morning_clouds">
+        <div className="cloud cloud1"></div>
+        <div className="cloud cloud2"></div>
+        <div className="cloud cloud3"></div>
+        <div className="cloud cloud4"></div>
+        <div className="cloud cloud5"></div>
+    </div>
+    };
+}
--- js/Components/MainWindow.js
+++ js/Components/MainWindow.js
@@ -6,12 +6,14 @@ import { Grass } from "./Grass";
 import {Moon} from "./Moon";
 import {Stars} from "./Stars";
 import {Sun} from "./Sun";
+import {Clouds} from "./Clouds";
 
 export class MainWindow extends Component {
     render() {
         return <div className="Main-window ">
             <Sky day={this.props.day}/>
             <Sun/>
+            <Clouds />
             <Farm/>
             <Grass />
             <Moon visible={!this.props.day}/>
