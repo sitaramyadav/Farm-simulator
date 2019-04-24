411175b027f5ce7a350b60987f3395a1e1777a1a
Aditi <in-aditi.lahare@in-aditilahare.local>
2019-04-24 14:22:56 +0530

    [Aditi] Created redux store to manage the state of app.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from  "./Components/App";
+import {createStore} from 'redux';
+import reducers from './reducer.js';
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />
-, document.getElementById("content"));
+const store = createStore(reducers);
+
+const app = (
+    <Provider store={store}>
+        <div>
+            <App/>
+        </div>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("content"));
